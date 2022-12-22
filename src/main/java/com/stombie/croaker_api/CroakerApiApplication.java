95ff252bf4d6bd44084eb1efacef50179f1ed3f5
StombieIT <stombie@yandex.ru>
2022-12-22 22:08:26 +0300

    initial commit

---
+++ src/main/java/com/stombie/croaker_api/CroakerApiApplication.java
@@ -0,0 +1,13 @@
+package com.stombie.croaker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CroakerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CroakerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stombie/croaker_api/CroakerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stombie.croaker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CroakerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
