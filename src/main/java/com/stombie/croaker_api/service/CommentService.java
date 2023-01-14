389f7aa2ca217bdf789d9a5762991da25bc1d85f
StombieIT <stombie@yandex.ru>
2023-01-14 14:10:57 +0300

    add comment service

---
+++ src/main/java/com/stombie/croaker_api/service/CommentService.java
@@ -0,0 +1,24 @@
+package com.stombie.croaker_api.service;
+
+import com.stombie.croaker_api.entity.Croak;
+import com.stombie.croaker_api.entity.User;
+import com.stombie.croaker_api.models.Reaction;
+import com.stombie.croaker_api.repo.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+    private final CommentRepository commentRepository;
+
+    @Autowired
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    public Reaction getReaction(Croak croak, User user) {
+        Long croakId = croak == null ? null : croak.getId();
+        Long userId = user == null ? null : user.getId();
+        return Reaction.of(commentRepository.getCountByCroakId(croakId), commentRepository.isActiveByCroakIdAndUserId(croakId, userId));
+    }
+}
