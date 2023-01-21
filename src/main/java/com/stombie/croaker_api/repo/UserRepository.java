709799df5b601beef7ce26e2ec8939612860b35f
StombieIT <stombie@yandex.ru>
2023-01-21 13:51:59 +0300

    add two methods for getting followers reaction

--- src/main/java/com/stombie/croaker_api/repo/UserRepository.java
+++ src/main/java/com/stombie/croaker_api/repo/UserRepository.java
@@ -2,7 +2,19 @@ package com.stombie.croaker_api.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.stombie.croaker_api.entity.User;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findUserByUsername(String username);
+
+    @Query("SELECT COUNT(u.followers) FROM User u WHERE u.id = :userId")
+    Long getFollowersCountByUserId(Long userId);
+
+    @Query("SELECT " +
+           "CASE WHEN COUNT(u) > 0 THEN TRUE ELSE FALSE END " +
+           "FROM User u " +
+           "JOIN u.followers f " +
+           "WHERE f.id = :checkerId " +
+           "AND u.id = :userId")
+    Boolean isActiveByUserIdAndCheckerId(Long userId, Long checkerId);
 }
