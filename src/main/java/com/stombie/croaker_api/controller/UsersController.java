10567f780ccf70051c250e7e75239506f93e669d
StombieIT <stombie@yandex.ru>
2023-01-21 18:58:07 +0300

    add register endpoint

--- src/main/java/com/stombie/croaker_api/controller/UsersController.java
+++ src/main/java/com/stombie/croaker_api/controller/UsersController.java
@@ -1,11 +1,17 @@
 package com.stombie.croaker_api.controller;
 
+import com.stombie.croaker_api.entity.User;
+import com.stombie.croaker_api.exception.UserAlreadyExistsException;
+import com.stombie.croaker_api.models.Message;
 import com.stombie.croaker_api.models.UserGetDto;
+import com.stombie.croaker_api.models.UserPostDto;
 import com.stombie.croaker_api.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +33,15 @@ public class UsersController {
                 .stream()
                 .map(UserGetDto::new)
                 .collect(Collectors.toList()));
+    }
+
+    @PostMapping(value = "/register")
+    public ResponseEntity<?> register(@RequestBody UserPostDto userPostDto) {
+        try {
+            User user = userService.saveUser(userPostDto.toUser());
+            return ResponseEntity.ok().body(UserGetDto.from(user));
+        } catch (UserAlreadyExistsException e) {
+            return ResponseEntity.badRequest().body(Message.of(e.getMessage()));
+        }
     }
 }
