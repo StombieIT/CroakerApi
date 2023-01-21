d020efae4041dfde1bcaf4667355a0f1b1c12bb9
StombieIT <stombie@yandex.ru>
2023-01-21 17:31:09 +0300

    fix profile one to one relationship model

--- src/main/java/com/stombie/croaker_api/entity/Profile.java
+++ src/main/java/com/stombie/croaker_api/entity/Profile.java
@@ -4,18 +4,21 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
-import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "profiles")
 public class Profile {
     @Id
+    @Column(name = "user_id")
     private Long id;
-    @OneToOne(fetch = FetchType.EAGER)
-    @PrimaryKeyJoinColumn
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "user_id")
     private User user;
     @Column(length = 512)
     private String description;
@@ -40,6 +43,46 @@ public class Profile {
 
     public Profile(User user, String description, Image backgroundImage) {
         this(user, description);
+        this.backgroundImage = backgroundImage;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Image getBackgroundImage() {
+        return backgroundImage;
+    }
+
+    public void setBackgroundImage(Image backgroundImage) {
         this.backgroundImage = backgroundImage;
     }
 }
