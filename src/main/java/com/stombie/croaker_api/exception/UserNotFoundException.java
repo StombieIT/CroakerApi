98c908d0ce1104d2cde8d253e6e9925e173a4163
StombieIT <stombie@yandex.ru>
2023-01-21 21:16:59 +0300

    add user not found exception

---
+++ src/main/java/com/stombie/croaker_api/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.stombie.croaker_api.exception;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
