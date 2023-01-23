6b4be0aee74d603efa6fc6555d9135ff46c8ee4d
StombieIT <stombie@yandex.ru>
2023-01-23 10:23:27 +0300

    fix tokens lifetime

--- src/main/java/com/stombie/croaker_api/Constants.java
+++ src/main/java/com/stombie/croaker_api/Constants.java
@@ -3,9 +3,10 @@ package com.stombie.croaker_api;
 import com.auth0.jwt.algorithms.Algorithm;
 
 public class Constants {
+    private static final long DAY_IN_MILLIS = 1000 * 60 * 60 * 24;
     public static final String SECRET = "ficko1234";
-    public static final long ACCESS_TOKEN_LIFETIME_IN_MILLIS = 1000 * 60 * 60;
-    public static final long REFRESH_TOKEN_LIFETIME_IN_MILLIS = ACCESS_TOKEN_LIFETIME_IN_MILLIS * 1000;
+    public static final long ACCESS_TOKEN_LIFETIME_IN_MILLIS = DAY_IN_MILLIS * 7;
+    public static final long REFRESH_TOKEN_LIFETIME_IN_MILLIS = DAY_IN_MILLIS * 365;
     public static final Algorithm ALGORITHM = Algorithm.HMAC256(SECRET.getBytes());
     public static final String[] ALLOWED_ORIGINS = {
         "http://localhost:3000/"
