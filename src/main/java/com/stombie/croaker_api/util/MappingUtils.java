6266ba6ee6d1fbcd0d692a0e37149f4fd27c824b
StombieIT <stombie@yandex.ru>
2023-01-23 16:44:36 +0300

    fix get image link method

--- src/main/java/com/stombie/croaker_api/util/MappingUtils.java
+++ src/main/java/com/stombie/croaker_api/util/MappingUtils.java
@@ -4,14 +4,26 @@ import com.stombie.croaker_api.entity.Image;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 public class MappingUtils {
-    public static String getLink(String filename) {
-        return ServletUriComponentsBuilder.fromCurrentContextPath().toUriString() + '/' + filename;
+    private static final String RESOURCE_IMAGES_RELATIVE_PATH = "/images";
+
+    public static String getResourceImageRelativePath(String filename) {
+        if (filename == null) {
+            return null;
+        }
+        return RESOURCE_IMAGES_RELATIVE_PATH + '/' + filename;
+    }
+
+    public static String getImageLink(String filename) {
+        if (filename == null) {
+            return null;
+        }
+        return ServletUriComponentsBuilder.fromCurrentContextPath().toUriString() + getResourceImageRelativePath(filename);
     }
 
     public static String getLink(Image image) {
         if (image == null) {
             return null;
         }
-        return getLink(image.getFilename());
+        return getImageLink(image.getFilename());
     }
 }
