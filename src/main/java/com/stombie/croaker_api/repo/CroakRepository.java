6b80c8f6c3cca3c2496ec69b1e7392b426f1534a
StombieIT <stombie@yandex.ru>
2023-01-23 10:24:55 +0300

    add method, that sorts all croaks of certain author by creation date

--- src/main/java/com/stombie/croaker_api/repo/CroakRepository.java
+++ src/main/java/com/stombie/croaker_api/repo/CroakRepository.java
@@ -4,6 +4,8 @@ import com.stombie.croaker_api.entity.Croak;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface CroakRepository extends JpaRepository<Croak, Long> {
     @Query("SELECT COUNT(c) FROM Croak c WHERE c.originalCroak.id = :originalCroakId")
     Long getCountByOriginalCroakId(Long originalCroakId);
@@ -13,4 +15,10 @@ public interface CroakRepository extends JpaRepository<Croak, Long> {
 
     @Query("SELECT CASE WHEN COUNT(c) > 0 THEN TRUE ELSE FALSE END FROM Croak c WHERE c.originalCroak.id = :originalCroakId AND c.author.id = :userId")
     Boolean isActiveByOriginalCroakIdAndUserId(Long originalCroakId, Long userId);
+
+    @Query("SELECT c " +
+           "FROM Croak c " +
+           "WHERE c.author.id = :authorId " +
+           "ORDER BY c.creationDate DESC ")
+    List<Croak> findAllByAuthorIdOrderingByCreationDateDesc(Long authorId);
 }
