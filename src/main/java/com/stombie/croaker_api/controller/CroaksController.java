1261ea4b0fb632b955eceba2ec5c7bf78142f924
StombieIT <stombie@yandex.ru>
2023-01-23 10:26:12 +0300

    add method which find all croaks of certain user

--- src/main/java/com/stombie/croaker_api/controller/CroaksController.java
+++ src/main/java/com/stombie/croaker_api/controller/CroaksController.java
@@ -1,7 +1,9 @@
 package com.stombie.croaker_api.controller;
 
 import com.stombie.croaker_api.entity.User;
+import com.stombie.croaker_api.exception.UserNotFoundException;
 import com.stombie.croaker_api.models.CroakGetDto;
+import com.stombie.croaker_api.models.Message;
 import com.stombie.croaker_api.models.Paginator;
 import com.stombie.croaker_api.service.CroakService;
 import com.stombie.croaker_api.service.UserService;
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,16 +30,20 @@ public class CroaksController {
         this.userService = userService;
     }
 
-    @GetMapping("/")
-    public ResponseEntity<Paginator<CroakGetDto>> index(@RequestParam(required = false, defaultValue = "1") int page,
-                                                        @RequestParam(required = false, defaultValue = "10") int limit,
-                                                        Authentication authentication) {
+    @GetMapping("/by-user-id/{userId}")
+    public ResponseEntity<?> byUserId(@PathVariable Long userId,
+                                      @RequestParam(required = false, defaultValue = "1") int page,
+                                      @RequestParam(required = false, defaultValue = "10") int limit,
+                                      Authentication authentication) {
         if (page <= 0 || limit <= 0) {
             return ResponseEntity.badRequest().build();
         }
-        User user = authentication == null ? null : userService.getUser(authentication.getName());
-        List<CroakGetDto> croaks = croakService.getSortedByCreationDateDescDtoList(user);
-        return ResponseEntity.ok()
-                .body(Paginator.of(croaks, page, limit));
+        User user = userService.findByAuthentication(authentication);
+        try {
+            List<CroakGetDto> croaks = croakService.getSortedByCreationDateDescDtoList(userId, user);
+            return ResponseEntity.ok().body(Paginator.of(croaks, page, limit));
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.badRequest().body(Message.of(e.getMessage()));
+        }
     }
 }
